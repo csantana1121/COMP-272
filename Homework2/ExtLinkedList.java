b4243625651749f1d7b950266954c15d095ccc5f
Christian Santana <christians1121@gmail.com>
2021-09-16 10:16:19 -0500

    fleshed out method

--- Homework2/ExtLinkedList.java
+++ Homework2/ExtLinkedList.java
@@ -1,9 +1,24 @@
 import java.util.*;
 public class ExtLinkedList<E> extends LinkedList<E>{
     public ExtLinkedList <E> secondHalfList(){
-        return null;
+        ExtLinkedList <E> secondhalf = new ExtLinkedList<E>();
+        int size = this.size();
+        int midpoint = size / 2;
+        ListIterator new_list = this.listIterator(midpoint);
+        // while(new_list.hasNext()){
+        //     System.out.println(new_list);
+        // }
+        return secondhalf; 
     }
     public static void main(String[] args) {
-        
+        ExtLinkedList<Integer> test = new ExtLinkedList<Integer>();
+        test.add(1);
+        test.add(2);
+        test.add(3);
+        test.add(4);
+        test.add(5);
+        System.out.println(test);
+        ExtLinkedList second = test.secondHalfList();
+        System.out.print(second);
     }
 }
