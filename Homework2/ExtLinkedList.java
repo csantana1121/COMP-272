7ba58f74f80c8706206c294a8c606797010111d3
Christian Santana <christians1121@gmail.com>
2021-09-16 11:07:08 -0500

    method for extracting odd index linkedlist elements

--- Homework2/ExtLinkedList.java
+++ Homework2/ExtLinkedList.java
@@ -4,11 +4,10 @@ public class ExtLinkedList<E> extends LinkedList<E>{
         ExtLinkedList <E> secondhalf = new ExtLinkedList<E>();
         int size = this.size();
         int midpoint;
-        if (size%2 == 0){
+        if (size%2 == 0)
             midpoint = size / 2;
-        }else{
+        else
             midpoint = (size / 2)+1; 
-        }
         for (int i = midpoint; i < size; i++){
             secondhalf.add(this.get(i));
         }
@@ -16,7 +15,13 @@ public class ExtLinkedList<E> extends LinkedList<E>{
     }
 
     public ExtLinkedList <E> oddList(){
-        return null;
+        ExtLinkedList <E> oddindexs = new ExtLinkedList<E>();
+        int size = this.size();
+        for (int i = 0; i < size;i++){
+            if ( i % 2 != 0)
+                oddindexs.add(this.get(i));
+        }
+        return oddindexs;
     }
 
     public ExtLinkedList <E> evenList(){
@@ -24,6 +29,7 @@ public class ExtLinkedList<E> extends LinkedList<E>{
     }
     public static void main(String[] args) {
         ExtLinkedList<Integer> test = new ExtLinkedList<Integer>();
+        ExtLinkedList<String> empty = new ExtLinkedList<String>();
         test.add(1);
         test.add(2);
         test.add(3);
@@ -31,7 +37,14 @@ public class ExtLinkedList<E> extends LinkedList<E>{
         test.add(5);
         //test.add(6); //comment in and out for even and odd number list
         ExtLinkedList second = test.secondHalfList();
+        ExtLinkedList secondempty = empty.secondHalfList();
         System.out.println(test);
-        System.out.print(second);
+        System.out.println(second);
+        System.out.println(secondempty);
+        ExtLinkedList oddindexes = test.oddList();
+        ExtLinkedList oddempty = empty.oddList();
+        System.out.println(oddindexes);
+        System.out.println(oddempty);
+
     }
 }
