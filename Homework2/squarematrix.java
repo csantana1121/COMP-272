988abf301c6fa2c43ed636ffc4cf5ab59e7e7612
Christian Santana <christians1121@gmail.com>
2021-09-16 12:11:27 -0500

    canvas complete

---
+++ Homework2/squarematrix.java
@@ -0,0 +1,16 @@
+public class squarematrix {
+
+    public void rowSums(int[][] arr){
+        
+    }
+
+    public void columnMins(int[][] arr){
+
+    }
+    public static void main(String[] args) {
+        squarematrix driver = new squarematrix();
+        int square[][] = {{3,2,5},{1,0,4}{5,6,7}};
+        driver.rowSums(square);
+        driver.columnMins(square);
+    }
+}
