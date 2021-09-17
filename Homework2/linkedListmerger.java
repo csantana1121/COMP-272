5ad7c79e03b0284020bf500e611d12d054cb2250
Csantana1121 <christians1121@gmail.com>
2021-09-17 16:58:07 -0500

    added test method frame

--- Homework2/linkedListmerger.java
+++ Homework2/linkedListmerger.java
@@ -6,6 +6,22 @@ public class linkedListmerger{
 
     }
     public static void main(String[] args) {
-        
+        LinkedList<String> firstlist = new LinkedList<String>();
+        LinkedList<String> secondlist = new LinkedList<String>();
+        firstlist.add("Apple");
+        firstlist.add("Banana");
+        firstlist.add("Grapes");
+        firstlist.add("Peaches");
+        firstlist.add("Watermelon");
+        secondlist.add("Alabama");
+        secondlist.add("Delaware");
+        secondlist.add("Illinois");
+        secondlist.add("New York");
+        secondlist.add("Wisconsin");
+        linkedListmerger driver = new linkedListmerger();
+        LinkedList<String> mergedlist = driver.Sortedmerge(firstlist, secondlist);
+        System.out.println(firstlist);
+        System.out.println(secondlist);
+        System.out.println(mergedlist);
     }
 }
