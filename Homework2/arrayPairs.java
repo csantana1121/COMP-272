01a3becf8695453faa3c39e92f7d4cf51a779288
Csantana1121 <christians1121@gmail.com>
2021-09-17 18:54:12 -0500

    tried different methods

--- Homework2/arrayPairs.java
+++ Homework2/arrayPairs.java
@@ -1,6 +1,22 @@
 public class arrayPairs {
     public void uniquePairs(int[] arr, int k){
-
+        int pointerOne = 0;
+        int pointerTwo = arr.length - 1;
+        String output = "";
+        while (pointerOne < pointerTwo) {
+            int target = Math.abs(arr[pointerOne] - arr[pointerTwo]);
+            // System.out.println(target);
+            if (target == k) {
+                output = "(" + arr[pointerOne] + "," + arr[pointerTwo] + "), ";
+                pointerOne++;
+            } else if (target < k) {
+                pointerOne++;
+            } else {
+                pointerTwo--;
+            }
+        }
+        output = output.substring(0, output.length() -2);
+        System.out.println(output);
     }
 
     public static void main(String[] args) {
