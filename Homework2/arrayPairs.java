9e0b495127e74022f859ac073c2dbb7b79ad733b
Csantana1121 <christians1121@gmail.com>
2021-09-17 18:03:54 -0500

    test method added

--- Homework2/arrayPairs.java
+++ Homework2/arrayPairs.java
@@ -4,6 +4,8 @@ public class arrayPairs {
     }
 
     public static void main(String[] args) {
-        
+        int[] intArray = new int[]{ 1,4,9,12, 6, 15, 5, 13,17 };
+        arrayPairs driver = new arrayPairs();
+        driver.uniquePairs(intArray, 3);
     }
 }
