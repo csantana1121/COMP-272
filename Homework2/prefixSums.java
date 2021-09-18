12b6e88bfafe5cab70f2d6695f34d97d0eea25c2
Csantana1121 <christians1121@gmail.com>
2021-09-18 15:23:52 -0500

    fixed prefixsums to accept empty linkedlist

--- Homework2/prefixSums.java
+++ Homework2/prefixSums.java
@@ -3,8 +3,12 @@ import java.util.*;
 public class prefixSums{
     
     public void prefixSums(LinkedList<Integer> input){
-        ListIterator <Integer> li = input.listIterator();
         int prefixsum = 0;
+        if(input.isEmpty()){
+            System.out.println(prefixsum);
+            return;
+        }
+        ListIterator <Integer> li = input.listIterator();
         String output = "";
         while(li.hasNext()){
             prefixsum += (int) li.next();
@@ -15,8 +19,12 @@ public class prefixSums{
     }
 
     public void reversePrefixSums(LinkedList<Integer> input){
-        Iterator li = input.descendingIterator();
         int prefixsum = 0;
+        if(input.isEmpty()){
+            System.out.println(prefixsum);
+            return;
+        }
+        Iterator li = input.descendingIterator();
         String output = "";
         while(li.hasNext()){
             prefixsum += (int) li.next();
@@ -29,6 +37,7 @@ public class prefixSums{
     public static void main(String[] args) {
         prefixSums driver = new prefixSums();
         LinkedList<Integer> test = new LinkedList<Integer>();
+        LinkedList<Integer> empty = new LinkedList<Integer>();
         test.add(5);
         test.add(3);
         test.add(2);
@@ -38,5 +47,7 @@ public class prefixSums{
         test.add(22);
         driver.prefixSums(test);
         driver.reversePrefixSums(test);
+        driver.prefixSums(empty);
+        driver.reversePrefixSums(empty);
     }
 }
