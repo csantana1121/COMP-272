eb929d268f6376602ff3f8cf1fd4597d132e6d2f
Csantana1121 <christians1121@gmail.com>
2021-09-24 14:45:38 -0500

    test method acded for MyLinkedList and ready for submission

--- Homework3/MyLinkedList.java
+++ Homework3/MyLinkedList.java
@@ -249,39 +249,6 @@ public class MyLinkedList<E>
                 return false;
     }
     
-    public static void main(String[] args) {
-        MyLinkedList<Integer> test = new MyLinkedList<Integer>();
-        MyLinkedList<Integer> test2 = new MyLinkedList<Integer>();
-        System.out.println(test.isEmpty());
-        test.addFirst(2);
-        test.addFirst(0);
-        test.addFirst(1);
-        test2.addFirst(2);
-        test2.addFirst(0);
-        test2.addFirst(2);
-        System.out.println(test.equals(test2));
-        test2.clear();
-        test.clear();
-        System.out.println(test.equals(test2));
-        test.printListForward();
-        test.printListBackward();
-        test.removeFirst();
-        test.removeFirst();
-        test.removeFirst();
-        test.addLast(3);
-        test.addLast(0);
-        test.addLast(5);
-        System.out.println("index 0 = " +test.get(0));
-        System.out.println("index 2 = " +test.get(2));
-        System.out.println("index 5 = " +test.get(5));
-        System.out.println(test.contains(0));
-        System.out.println(test.contains(6));
-        test.printListForward();
-        test.clear();
-        System.out.println(test.contains(0));
-        test.printListForward();
-    }
-    
 }
     
     
