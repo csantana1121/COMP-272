b826fa361ba3eb62edc2383ceb5cf31d6456721a
Christian Santana <christians1121@gmail.com>
2021-09-30 15:32:30 -0500

    first thoughts

--- Homework4/BinarySearchTree.java
+++ Homework4/BinarySearchTree.java
@@ -1,20 +1,38 @@
 public class BinarySearchTree<E extends Comparable<E>> extends BinaryTree<E> {
 
+    int size;
+    Node<E> root;
     public BinarySearchTree() {
-       
+       size = 0;
+       root = null;
    }
    public BinarySearchTree(E val) {
-       
+       root = new Node(val);
+       size = 1;
    }
    
 // returns true if BST has val else false.
    public boolean contains (E val) {
-      
+        Node<E> node  = root;
+        if(node == null)
+            return false;
+        else if (node.getInfo()== val){
+            return true;
+        } else {
+            
+        }
        
    }
 // inserts val at the right place satisfying search tree properties, should handle if the tree is empty
 // if value is already there then donâ€™t insert it again
    public void insert(E val) {
+        
+        Node<E> node = new Node(val);
+        if (size == 0)
+            root = node;
+        else if (!this.contains(val)){
+            
+        }
 
 
         }
