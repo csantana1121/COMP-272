6823f526e5291cc7810b2c87df06d3da6aa5c413
Christian Santana <christians1121@gmail.com>
2021-10-07 15:24:48 -0500

    working version

--- Homework5/MaxHeap.java
+++ Homework5/MaxHeap.java
@@ -84,7 +84,7 @@ public class MaxHeap<E extends Comparable<E>> extends ArrayList<E>   {
     public void heapSort(List<E> list){
         this.buildHeap(list);
         while(arr.size()>0){
-            this.removeHeap();
+            System.out.println(this.removeHeap());
         }
     }
     
@@ -104,6 +104,17 @@ public class MaxHeap<E extends Comparable<E>> extends ArrayList<E>   {
 
     public static void main(String[] args) {
         MaxHeap<Integer> heaper = new MaxHeap<>();
+        MaxHeap<Integer> builder = new MaxHeap<>();
+        ArrayList<Integer> test = new ArrayList<>();
+        test.add(213);
+        test.add(32);
+        test.add(2143);
+        test.add(21);
+        test.add(1);
+        test.add(8);
+        test.add(540);
+        builder.heapSort(test);
+        builder.buildHeap(test);
         heaper.addHeap(10);
         System.out.println("max:" + heaper.removeHeap());
         heaper.addHeap(13);
@@ -113,6 +124,8 @@ public class MaxHeap<E extends Comparable<E>> extends ArrayList<E>   {
         heaper.addHeap(8);
         heaper.addHeap(1);
         System.out.println("max:" + heaper.removeHeap());
+        System.out.println(heaper.arr);
+        heaper.heapMerge(builder);
         System.out.println(heaper.arr);
         System.out.println(heaper.size());
     }
