c3fbc71d274566e64ae1f8b2d629cfa78c0b70fa
Christian Santana <christians1121@gmail.com>
2021-10-07 15:26:38 -0500

    moved test methods to test.java

--- Homework5/MaxHeap.java
+++ Homework5/MaxHeap.java
@@ -101,33 +101,5 @@ public class MaxHeap<E extends Comparable<E>> extends ArrayList<E>   {
             this.addHeap(element);
         }
     }
-
-    public static void main(String[] args) {
-        MaxHeap<Integer> heaper = new MaxHeap<>();
-        MaxHeap<Integer> builder = new MaxHeap<>();
-        ArrayList<Integer> test = new ArrayList<>();
-        test.add(213);
-        test.add(32);
-        test.add(2143);
-        test.add(21);
-        test.add(1);
-        test.add(8);
-        test.add(540);
-        builder.heapSort(test);
-        builder.buildHeap(test);
-        heaper.addHeap(10);
-        System.out.println("max:" + heaper.removeHeap());
-        heaper.addHeap(13);
-        heaper.addHeap(1);
-        heaper.addHeap(321);
-        heaper.addHeap(43);
-        heaper.addHeap(8);
-        heaper.addHeap(1);
-        System.out.println("max:" + heaper.removeHeap());
-        System.out.println(heaper.arr);
-        heaper.heapMerge(builder);
-        System.out.println(heaper.arr);
-        System.out.println(heaper.size());
-    }
     
 }
--- Homework5/test.java
+++ Homework5/test.java
@@ -1,6 +1,30 @@
+import java.util.*;
 public class test {
     public static void main(String[] args) {
         MaxHeap<Integer> heaper = new MaxHeap<>();
-        System.out.println(heaper.findMax());
+        MaxHeap<Integer> builder = new MaxHeap<>();
+        ArrayList<Integer> test = new ArrayList<>();
+        test.add(213);
+        test.add(32);
+        test.add(2143);
+        test.add(21);
+        test.add(1);
+        test.add(8);
+        test.add(540);
+        builder.heapSort(test);
+        builder.buildHeap(test);
+        heaper.addHeap(10);
+        System.out.println("max:" + heaper.removeHeap());
+        heaper.addHeap(13);
+        heaper.addHeap(1);
+        heaper.addHeap(321);
+        heaper.addHeap(43);
+        heaper.addHeap(8);
+        heaper.addHeap(1);
+        System.out.println("max:" + heaper.removeHeap());
+        System.out.println(heaper.arr);
+        heaper.heapMerge(builder);
+        System.out.println(heaper.arr);
+        System.out.println(heaper.size());
     }
 }
