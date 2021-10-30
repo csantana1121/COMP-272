887ddcf91e04bd6741d07c3acce9a20c6512c525
Csantana1121 <christians1121@gmail.com>
2021-10-30 12:39:55 -0500

    canvas for Homework6

---
+++ Homework6/MyHashSet.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.security.*;
+import java.io.*;
+
+public class MyHashSet<E> {
+    
+    ArrayList<LinkedList<String>> hashtable = new ArrayList<LinkedList<String>>();
+
+    public static void main(String[] args) {
+        
+    }
+}
