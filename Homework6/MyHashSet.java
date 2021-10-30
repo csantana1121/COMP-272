a7be22aa2d11e00d230ba34361363f7066261bc7
Csantana1121 <christians1121@gmail.com>
2021-10-30 14:12:25 -0500

    progress so far

--- Homework6/MyHashSet.java
+++ Homework6/MyHashSet.java
@@ -5,9 +5,15 @@ import java.io.*;
 public class MyHashSet<E> {
     
     ArrayList<LinkedList<String>> hashtable;
+    int collisions;
+    int tableSize;
+    int size;
 
     public MyHashSet(int size) {
         hashtable = new ArrayList<LinkedList<String>>(size);
+        collisions = 0;
+        tableSize = size;
+        size = 0;
     }
 
     public int hashCode(Object key) {
@@ -16,25 +22,50 @@ public class MyHashSet<E> {
     }
 
     public void HashMethod1(String s){
-        int h = Math.abs(s.hashCode()) % hashtable.size();
-        LinkedList<String> hold = hashtable.get(h);
-        hold.add(s);
-        hashtable.add(h, hold);
+        int h = Math.abs(s.hashCode() % tableSize);
+        if (size >0){
+            LinkedList<String> hold = hashtable.get(h);
+        // try{
+        // } catch (IndexOutOfBoundsException ex){
+        //     hold = new LinkedList<>();
+        // }
+            if (hold.size() > 0){
+                collisions++;
+            }
+            hold.add(s);
+            hashtable.add(h, hold);
+        } else{
+            LinkedList<String> hold = new LinkedList<>();
+            hold.add(s);
+            hashtable.add(h, hold);
+        }
     }
 
     public void HashMethod2(String s){
-        int h = Math.abs(hashCode(s)%hashtable.size());
+        int h = Math.abs(hashCode(s)%tableSize);
         LinkedList<String> hold = hashtable.get(h);
+        if (hold.size() > 0){
+            collisions++;
+        }
         hold.add(s);
         hashtable.add(h, hold);
     }
     public static void main(String[] args) {
         MyHashSet<String> method1 = new MyHashSet<>(262127);
+        System.out.println(method1.tableSize);
+        System.out.println(method1.hashtable.size());
         MyHashSet<String> method2 = new MyHashSet<>(262127);
         File text = new File("EnglishWordList.txt");
-        Scanner scanner = new Scanner(text);
-        while(scanner.hasNextLine()){
-            System.out.println(scanner.nextLine());
+        try{
+            Scanner scanner = new Scanner(text);
+            while(scanner.hasNextLine()){
+                String word = scanner.nextLine();
+                System.out.println(word);
+                method1.HashMethod1(word);
         }
+        } catch ( FileNotFoundException ex){
+            ex.printStackTrace();
+        }
+        System.out.println(method1.collisions);
     }
 }
