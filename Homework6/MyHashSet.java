75844eab455c7b10ac15bc36cca48c4c291988c6
Csantana1121 <christians1121@gmail.com>
2021-10-31 01:03:03 -0500

    works for method 1

--- Homework6/MyHashSet.java
+++ Homework6/MyHashSet.java
@@ -7,13 +7,15 @@ public class MyHashSet<E> {
     ArrayList<LinkedList<String>> hashtable;
     int collisions;
     int tableSize;
-    int size;
 
     public MyHashSet(int size) {
         hashtable = new ArrayList<LinkedList<String>>(size);
+        for( int i = 0; i < size; i++){
+            LinkedList list = new LinkedList<>();
+            hashtable.add(list);
+        }
         collisions = 0;
         tableSize = size;
-        size = 0;
     }
 
     public int hashCode(Object key) {
@@ -22,23 +24,13 @@ public class MyHashSet<E> {
     }
 
     public void HashMethod1(String s){
-        int h = Math.abs(s.hashCode() % tableSize);
-        if (size >0){
-            LinkedList<String> hold = hashtable.get(h);
-        // try{
-        // } catch (IndexOutOfBoundsException ex){
-        //     hold = new LinkedList<>();
-        // }
-            if (hold.size() > 0){
-                collisions++;
-            }
-            hold.add(s);
-            hashtable.add(h, hold);
-        } else{
-            LinkedList<String> hold = new LinkedList<>();
-            hold.add(s);
-            hashtable.add(h, hold);
+        int h = Math.abs(s.hashCode()) % tableSize;
+        LinkedList<String> hold = hashtable.get(h);
+        if (hold.size() > 0){
+            collisions++;
         }
+        hold.add(s);
+        hashtable.set(h,hold);
     }
 
     public void HashMethod2(String s){
@@ -48,24 +40,61 @@ public class MyHashSet<E> {
             collisions++;
         }
         hold.add(s);
-        hashtable.add(h, hold);
+        hashtable.set(h, hold);
     }
+
+    public double calavgsize(){
+        double avg = 0;
+        for(int i = 0; i <tableSize;i++){
+            avg += hashtable.get(i).size();
+        }
+        avg = avg / tableSize;
+        return avg;
+    }
+
+    public int hashCode(String s) {
+        byte[] sb=s.getBytes();
+        byte[] key=  null;
+         try {
+         MessageDigest md = MessageDigest.getInstance("SHA-256");
+         key=md.digest(sb);
+         }
+         catch (NoSuchAlgorithmException e) {
+         e.printStackTrace();
+         }
+        BitSet bs = BitSet.valueOf(key);
+        // using the BitSet bs, you will extract 18 bits based on the
+        //get() method of BitSet. The 18 bits are extracted at the first
+        //18 prime numbers 2, 7,17,29,41,53,67,79,97,107,127,139,157,
+        //173,191, 199,227,239. Put the bits together in that //order to
+        //form an integer and return it. That will be the hash //value of
+        //the key that you can use in the table of size 2^18.
+        return  0;
+        }
     public static void main(String[] args) {
         MyHashSet<String> method1 = new MyHashSet<>(262127);
         System.out.println(method1.tableSize);
         System.out.println(method1.hashtable.size());
         MyHashSet<String> method2 = new MyHashSet<>(262127);
         File text = new File("EnglishWordList.txt");
+        HashSet<String> hs = new HashSet<String>();
         try{
             Scanner scanner = new Scanner(text);
             while(scanner.hasNextLine()){
                 String word = scanner.nextLine();
-                System.out.println(word);
-                method1.HashMethod1(word);
+                hs.add(word);
         }
         } catch ( FileNotFoundException ex){
             ex.printStackTrace();
         }
+        for(String word: hs){
+            method1.HashMethod1(word);
+            method2.HashMethod2(word);
+        }
+        System.out.println(hs.size());
         System.out.println(method1.collisions);
+        System.out.println(method1.calavgsize());
+        System.out.println("Method 2 " +method2.collisions);
+        System.out.println(method2.calavgsize());
     }
 }
