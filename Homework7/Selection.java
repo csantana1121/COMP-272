6e591e3176aaf0a972d06510017081ac1e179f08
Csantana1121 <christians1121@gmail.com>
2021-11-05 23:17:16 -0500

    created Homework 7

---
+++ Homework7/Selection.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Selection <E extends Comparable<E>> {
+    int k;
+    ArrayList<E> input ; // this holds the values of type E from which your code will find kth largest.
+    // constructors
+    public Selection(List<E> list){
+        input.addAll(list);
+    }
+    // algorithm 1 methods -- implement 1B
+    // algorithm 2 methods -- 6A -- change the algorithm to do kth largest not kth smallest that is described here
+    // algorithm 3 methods – 6B
+
+    public static void main(String[] args) {
+        
+    }
+    }
