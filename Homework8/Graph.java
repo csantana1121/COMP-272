0201efa67b8018e583981c0590f4d2c989a1bfbd
Csantana1121 <christians1121@gmail.com>
2021-11-14 13:04:57 -0600

    starting version

--- Homework8/Graph.java
+++ Homework8/Graph.java
@@ -1,54 +1,54 @@
 import java.util.*;
 public class Graph
 {
-    int numVertex;
-    int numEdge;
-    ArrayList<ArrayList<Integer>> graph;
+    // int numVertex;
+    // int numEdge;
+    // ArrayList<ArrayList<Integer>> graph;
     
-    public Graph () {
-        numVertex =0;
-        numEdge =0;
-        graph = new ArrayList<>();
+    // public Graph () {
+    //     numVertex =0;
+    //     numEdge =0;
+    //     graph = new ArrayList<>();
         
-    }
+    // }
     
-    public Graph(int vertexCount) {
+    // public Graph(int vertexCount) {
         
-        numVertex=vertexCount;
-        numEdge=0;
-        graph = new ArrayList<>(numVertex);
-        for (int i=0;i<numVertex;i++)
-        graph.add(new ArrayList<>());
-    }
+    //     numVertex=vertexCount;
+    //     numEdge=0;
+    //     graph = new ArrayList<>(numVertex);
+    //     for (int i=0;i<numVertex;i++)
+    //     graph.add(new ArrayList<>());
+    // }
     
    
     
-    public ArrayList<Integer> getNeighbors(int u) {
+    // public ArrayList<Integer> getNeighbors(int u) {
         
-        return graph.get(u);
-    }
+    //     return graph.get(u);
+    // }
     
-    public boolean edgePresent(int u, int v) {
+    // public boolean edgePresent(int u, int v) {
       
-        return (graph.get(u).contains(v));
+    //     return (graph.get(u).contains(v));
         
-    }
+    // }
     
-    public void addEdge(int u, int v) {
-        // lets assume that the vertices are already created.
-       if (u>=0 && u<numVertex && v>=0 && v<numVertex) { 
-         if (!edgePresent(u,v))
-        graph.get(u).add(v);
+    // public void addEdge(int u, int v) {
+    //     // lets assume that the vertices are already created.
+    //    if (u>=0 && u<numVertex && v>=0 && v<numVertex) { 
+    //      if (!edgePresent(u,v))
+    //     graph.get(u).add(v);
         
-         if (!edgePresent(v,u))
-        graph.get(v).add(u);
-        numEdge++;
-    }
-    else throw new IndexOutOfBoundsException();
+    //      if (!edgePresent(v,u))
+    //     graph.get(v).add(u);
+    //     numEdge++;
+    // }
+    // else throw new IndexOutOfBoundsException();
     
     
         
-    }
+    // }
     
     
 }
